30ffd86adaf45fbf075123b2e165d809025127ca
Vadim Provkov <newprovkov@yandex.ru>
2024-02-05 22:14:55 +0300

    third

--- conftest.py
+++ conftest.py
@@ -1,5 +1,11 @@
 import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 
 def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
