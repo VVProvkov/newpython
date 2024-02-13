f014b413b5ae0237a0994739261284ad7b1c769f
VVProvkov <newprovkov@yandex.ru>
2024-02-13 09:54:53 +0300

    livecoding

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
 import pytest
+import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture(scope='function')
+
+
+#@pytest.fixture(scope='function')
 
 def driver():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome()
     driver.maximize_window()
-    yield driver
+    #yield driver
+    #url="https://metanit.com/"
+    driver.get(url)
+    #time.sleep(3)
     driver.quit()
+
+driver()
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -8,9 +8,7 @@ sys.path.append("../newpython")
 from pages.base_page import BasePage
 from conftest import driver
 
-driver2 = driver
-
 def test(driver):
-    page = BasePage(driver2, 'https://www.google.com')
+    page = BasePage(driver, 'https://www.google.com')
     page.open()
     time.sleep(3)
