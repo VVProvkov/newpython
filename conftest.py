c95269d6f3d67783baa14e2819ffe2135f54dd1e
newprovkov@yandex.ru <newprovkov@yandex.ru>
2024-02-19 22:47:06 +0300

    docker

--- conftest.py
+++ conftest.py
@@ -8,9 +8,9 @@ from selenium.webdriver.chrome.options import Options
 
 def driver(request):
     options = Options() 
-    #options.add_argument("--headless")
-    #options.add_argument("--no-sandbox")
-    #options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--windows-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
